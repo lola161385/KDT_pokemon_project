03a883756759c1151dbe9024ff5a7488f90fada1
LeeHyeongjun01 <joon010315@naver.com>
2025-01-27 11:11:34 +0900

    TrainerLocation 클래스 수정

--- src/main/java/pokemon/TrainerLocation.java
+++ src/main/java/pokemon/TrainerLocation.java
@@ -1,19 +1,29 @@
 package pokemon;
 
 public class TrainerLocation {
-    private static String trainerLocation = "Kanto"; // 트레이너의 현재 마을위치
-
-    public TrainerLocation(String trainerLocation) { // 다른 마을로
-        this.trainerLocation = trainerLocation;
-    }
-    public enum TownNames{
+    // TownNames enum 정의
+    public enum TownNames {
         Sinnoh,
         Kanto,
         Hana,
         MoonRiseGarden
     }
 
-    public String getTownName() {
+    private TownNames trainerLocation;
+
+    // 생성자
+    public TrainerLocation() {
+        this.trainerLocation = TownNames.Kanto; // 초기 위치를 Kanto로 설정
+    }
+
+    // 현재 위치 반환
+    public TownNames getTrainerLocation() {
         return trainerLocation;
+    }
+
+    // 위치 변경 메서드
+    public void setTrainerLocation(TownNames newLocation) {
+        this.trainerLocation = newLocation;
+        System.out.println("이동 완료! 현재 위치: " + newLocation);
     }
 }
